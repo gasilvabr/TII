857fcb15d7362e57b89f5a63f955cd8ae0354947
George Azevedo da Silva <gasilvabr@hotmail.com>
2021-01-14 18:14:51 -0300

    Inclusão de projeto

---
+++ WebApplicationTIIAula/WebApplicationTIIAula/Controllers/CategoriasController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTIIAula.Models;
+
+namespace WebApplicationTIIAula.Controllers
+{
+    public class CategoriasController : Controller
+    {
+
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+           new Categoria() { CategoriaId = 1, Nome = "Notebooks"},
+           new Categoria() { CategoriaId = 2, Nome = "Monitores"},
+           new Categoria() { CategoriaId = 3, Nome = "Impressoras"},
+           new Categoria() { CategoriaId = 4, Nome = "Mouses"},
+           new Categoria() { CategoriaId = 5, Nome = "Desktops"}
+        };
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+
+        // GET: Categorias
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplicationTIIAula/WebApplicationTIIAula/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationTIIAula.MvcApplication" Language="C#" %>
